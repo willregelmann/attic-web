741e1131bf510f1e6864171c131c663b27a63dc3
Will Regelmann <will@regelmann.net>
2025-11-16 10:35:36 -0500

    test: add data-testid attributes for wishlist tests
    - Add data-testid='wishlist-heading' to h1 in all views (loading, empty, regular)
    - Add data-testid='empty-wishlist' to empty state container
    - Supports wishlist-items.spec.ts test selectors

--- src/components/WishlistView.jsx
+++ src/components/WishlistView.jsx
@@ -50,7 +50,7 @@ function WishlistView() {
       <div className="wishlist-view">
         <div className="wishlist-header">
           <div className="header-content">
-            <h1>My Wishlist</h1>
+            <h1 data-testid="wishlist-heading">My Wishlist</h1>
             <p>Items you want to add to your collection</p>
           </div>
         </div>
@@ -80,11 +80,11 @@ function WishlistView() {
       <div className="wishlist-view">
         <div className="wishlist-header">
           <div className="header-content">
-            <h1>My Wishlist</h1>
+            <h1 data-testid="wishlist-heading">My Wishlist</h1>
             <p>Items you want to add to your collection</p>
           </div>
         </div>
-        <div className="wishlist-empty">
+        <div className="wishlist-empty" data-testid="empty-wishlist">
           <svg className="empty-icon" viewBox="0 0 24 24" fill="none">
             <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
                   stroke="currentColor" strokeWidth="2" fill="none"/>
@@ -103,7 +103,7 @@ function WishlistView() {
     <div className="wishlist-view">
       <div className="wishlist-header">
         <div className="header-content">
-          <h1>My Wishlist</h1>
+          <h1 data-testid="wishlist-heading">My Wishlist</h1>
           <p>Items you want to add to your collection</p>
         </div>
         <div className="wishlist-count">
